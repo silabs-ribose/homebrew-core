c0f0a4d9f7f55cb79985ff070048bf4c7c51638b
Rui Chen <rui@chenrui.dev>
2025-09-15 18:42:35 -0400

    raxml-ng: fix cmake 4 compatibility
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/raxml-ng.rb
+++ Formula/r/raxml-ng.rb
@@ -29,8 +29,10 @@ class RaxmlNg < Formula
   end
 
   def install
-    args = std_cmake_args + ["-DUSE_GMP=ON"]
-    system "cmake", "-S", ".", "-B", "build", *args
+    args = %w[-DUSE_GMP=ON]
+    # Workaround to build with CMake 4
+    args << "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
